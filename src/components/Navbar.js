9bbc1565955e8970c2abbcfd32b19c08dc935512
cdmejiam94 <cdmejiam94@gmail.com>
2022-03-14 17:57:29 -0600

    configuracion de componente de busqueda; finaliza v1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = ({ brand }) => {
   return (
-    <nav className="navbar navbar-darg bg-dark">
+    <nav className="navbar navbar-dark bg-dark">
       <div className="container">
         <a className="navbar-brand text-uppercase link-light" href="/">{ brand }</a>
       </div>
